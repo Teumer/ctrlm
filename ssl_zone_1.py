c6f481c65611b02d79d9a41026137e945b8936b5
jteumer <joe.teumer@gmail.com>
2020-08-31 15:05:16 -0500

    correct formatting

--- ssl_zone_1.py
+++ ssl_zone_1.py
@@ -22,84 +22,84 @@ class SSL:
 
     def run_ctmkeytool(self):
         # Private key file (.pem) and the CSR file (.csr)
-        return "su - em1 -c \"{utility} \
-                -create_csr \
-                -password {password} \
-                -conf_file {configuration} \
-                -out {hostname}\"".format(
+        return "su - em1 -c {utility} " \
+               "-create_csr " \
+               "-password {password} " \
+               "-conf_file {configuration} " \
+               "-out {hostname}".format(
                 utility=self.ctmkeytool_em,
                 password=self.password,
                 configuration=self.zone_1_conf,
                 hostname=self.hostname
-        )
+                )
 
     def run_create_ca_key(self):
         # Create CA private key with DES in ede cbc mode (168 bit key)
-        return "su - em1 -c \"openssl genrsa \
-                -des3 \
-                -passout pass:{password} \
-                -out {ca_key} \
-                4096\"".format(
-                password=self.password,
-                ca_key=self.ca_key
-        )
+        return "su - em1 -c \"openssl genrsa " \
+                "-des3 " \
+                "-passout pass:{password} " \
+                "-out {ca_key} " \
+                "4096\"".format(
+                 password=self.password,
+                 ca_key=self.ca_key
+                 )
 
     def run_create_ca_certificate(self):
         # Create CA certificate
-        return "su - em1 -c \"openssl req -x509 \
-               -new \
-               -key \
-               {ca_key} \
-               -sha256 \
-               -days 1825 \
-               -passin pass:{password} \
-               -subj {subject} \
-               -out {ca_cert}\"".format(
+        return "su - em1 -c \"openssl req -x509 " \
+               "-new " \
+               "-key " \
+               "{ca_key} " \
+               "-sha256 " \
+               "-days 1825 " \
+               "-passin pass:{password} " \
+               "-subj {subject} " \
+               "-out {ca_cert}\"".format(
                 ca_key=self.ca_key,
                 password=self.password,
                 subject=self.subject,
                 ca_cert=self.ca_cert
-        )
+                )
 
     def run_create_domain_certificate(self):
         # Create domain certificate
-        return "su - em1 -c \"openssl x509 \
-                -req \
-                -in {zone_1_csr} \
-                -out {zone_1_cert} \
-                -CA {ca_cert} \
-                -CAkey {ca_key} \
-                -CAcreateserial \
-                -passin pass:{password} \
-                -extfile {ext_file} \
-                -extensions req_ext\"".format(
-                zone_1_csr=self.zone_1_csr,
-                zone_1_cert=self.zone_1_cert,
-                ca_cert=self.ca_cert,
-                ca_key=self.ca_key,
-                password=self.password,
-                ext_file=self.zone_1_conf
-        )
+        return "su - em1 -c \"openssl x509 " \
+                "-req " \
+                "-in {zone_1_csr} " \
+                "-out {zone_1_cert} " \
+                "-CA {ca_cert} " \
+                "-CAkey {ca_key} " \
+                "-CAcreateserial " \
+                "-passin pass:{password} " \
+                "-extfile {ext_file} " \
+                "-extensions req_ext\"".format(
+                 zone_1_csr=self.zone_1_csr,
+                 zone_1_cert=self.zone_1_cert,
+                 ca_cert=self.ca_cert,
+                 ca_key=self.ca_key,
+                 password=self.password,
+                 ext_file=self.zone_1_conf
+                )
 
     def run_create_tomcat_keystore(self):
         # Create the tomcat.p12 keystore file
-        return "su - em1 -c \"openssl pkcs12 \
-                -inkey {zone_1_key} \
-                -in {zone_1_cert} \
-                -passin pass:{password} \
-                -export \
-                -passout pass:{password} \
-                -CAfile {ca_key} \
-                -chain \
-                -out tomcat.p12 \
-                -name {hostname}-keystore \
-                -caname {hostname}-ca\"".format(
-                zone_1_key=self.zone_1_key,
-                zone_1_cert=self.zone_1_cert,
-                password=self.password,
-                ca_key=self.ca_key,
-                hostname=self.hostname
-        )
+        return "su - em1 -c \"openssl pkcs12 " \
+                "-inkey {zone_1_key} " \
+                "-in {zone_1_cert} " \
+                "-passin pass:{password} " \
+                "-export " \
+                "-passout pass:{password} " \
+                "-CAfile {ca_key} " \
+                "-chain " \
+                "-out tomcat.p12 " \
+                "-name {hostname}-keystore " \
+                "-caname {hostname}-ca\"".format(
+                 zone_1_key=self.zone_1_key,
+                 zone_1_cert=self.zone_1_cert,
+                 password=self.password,
+                 ca_key=self.ca_key,
+                 hostname=self.hostname
+                 )
 
 
 """
